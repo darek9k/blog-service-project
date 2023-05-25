5c5315ee20e380784c1f69853f0ce5b79a491683
darek9k <darecki9k@gmail.com>
2023-05-25 14:50:40 +0200

    Startup files from the course.

---
+++ src/main/java/darek9k/BlogServiceApplication.java
@@ -0,0 +1,13 @@
+package darek9k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/darek9k/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package darek9k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
