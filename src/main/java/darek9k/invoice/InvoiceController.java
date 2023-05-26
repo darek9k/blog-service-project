6b06b59fc0ea8329ad697fffe8956e2424ecdaa4
darek9k <darecki9k@gmail.com>
2023-05-26 14:52:51 +0200

    Last touches.

--- src/main/java/darek9k/invoice/InvoiceController.java
+++ src/main/java/darek9k/invoice/InvoiceController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/invoice")
@@ -21,16 +23,21 @@ public class InvoiceController {
     }
     @GetMapping
     public void read(){
+        //R
         Optional<Invoice> invoiceOptional = invoiceRepository.findById(1L);
         Invoice invoice = invoiceOptional.get();
         System.out.println(invoice);
 
+        //D
         invoiceRepository.delete(invoice);
-
         invoiceRepository.deleteById(2L);
+        invoiceRepository.deleteAllById(Set.of(12L,15L));
 
+        //List
         invoiceRepository.findAll().forEach(System.out::println);
+        invoiceRepository.findAllById(List.of(18L,19L)).forEach(System.out::println);
 
+        //U
         Optional<Invoice> optionalInvoice = invoiceRepository.findById(10L);
         Invoice invoice10 = optionalInvoice.get();
         invoice10.setBuyer("Arek");
