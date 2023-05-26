8ccea4054175d3107bd99d7fbae8440b128109b2
darek9k <darecki9k@gmail.com>
2023-05-26 10:35:23 +0200

    Change in properties. Writing methods to GET.

--- src/main/java/darek9k/post/PostController.java
+++ src/main/java/darek9k/post/PostController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/post")
@@ -23,6 +25,23 @@ public class PostController {
         Optional<Post> optionalPost = postRepository.findById(5L);
         Post post = optionalPost.get();
         System.out.println(post);
+
+        // -----------------------
+
+        postRepository.delete(post);
+
+        postRepository.deleteById(1L);
+
+        postRepository.deleteAllById(Set.of(8L, 19L, 11L));
+
+        // ------------------------
+        //Pull everything out // we display on the console using lambda
+        System.out.println("-----------findAll-------------");
+        postRepository.findAll().forEach(System.out::println);
+        System.out.println("-----------findAll-------------");
+        postRepository.findAllById(List.of(8L, 2L, 3L)).forEach(System.out::println);
+
+
     }
 
     @PostMapping
