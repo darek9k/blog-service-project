950d5ba45a2a976593f270cbd01ae11f86bf3039
darek9k <darecki9k@gmail.com>
2023-05-26 08:44:24 +0200

    Create a new entity. Invoice class.

---
+++ src/main/java/darek9k/invoice/Invoice.java
@@ -0,0 +1,23 @@
+package darek9k.invoice;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdDate;
+
+    private LocalDate paymentDate;
+
+    private String buyer;
+
+    private String seller;
+    @Enumerated(EnumType.STRING)
+    private InvoiceStatus status;
+}
