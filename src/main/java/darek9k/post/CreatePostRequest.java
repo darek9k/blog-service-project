6aeaf27b37d0464f8b8befdd3943130356241232
darek9k <darecki9k@gmail.com>
2023-05-27 06:27:21 +0200

    Rebuilding the create / POST method. Use of changed POST entity constructor. We set the createDate and createStatus fields rigidly in the constructor. We create CreatePostRequest classes to accept partial data into the POST entity.

---
+++ src/main/java/darek9k/post/CreatePostRequest.java
@@ -0,0 +1,36 @@
+package darek9k.post;
+
+import java.time.LocalDateTime;
+
+public class CreatePostRequest {
+    private final String text;
+
+    private final PostScope scope;
+
+    private final String author;
+
+    private final LocalDateTime publicationDate;
+
+    public CreatePostRequest(String text, PostScope scope, String author, LocalDateTime publicationDate) {
+        this.text = text;
+        this.scope = scope;
+        this.author = author;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public PostScope getScope() {
+        return scope;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getPublicationDate() {
+        return publicationDate;
+    }
+}
