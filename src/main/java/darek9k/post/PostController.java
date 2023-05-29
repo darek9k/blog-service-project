b601ca76515c48e0eb6be2ab3045b150b5a19beb
darek9k <darecki9k@gmail.com>
2023-05-29 06:02:45 +0200

    Validation error handling in the controller layer. Return ResponseEntity with validation error handling and send to response.

--- src/main/java/darek9k/post/PostController.java
+++ src/main/java/darek9k/post/PostController.java
@@ -1,7 +1,15 @@
 package darek9k.post;
 
 import jakarta.validation.Valid;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -22,4 +30,16 @@ public class PostController {
         postService.create(postRequest);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationEx(MethodArgumentNotValidException ex){
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> System.out.println(fieldError.getField() + ":" + fieldError.getDefaultMessage()));
+
+        Map<String, String> fieldsErrorMap = new HashMap<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            if (fieldsErrorMap.put(fieldError.getField(), fieldError.getDefaultMessage()) != null) {
+                throw new IllegalStateException("Duplicate key");
+            }
+        }
+return ResponseEntity.badRequest().body(fieldsErrorMap);
+    }
 }
