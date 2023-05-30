5818be54dacb9179a3758a9d346d78d6e1435509
darek9k <darecki9k@gmail.com>
2023-05-30 04:40:19 +0200

    Last touches

--- src/main/java/darek9k/post/UpdatePostRequest.java
+++ src/main/java/darek9k/post/UpdatePostRequest.java
@@ -1,11 +1,8 @@
 package darek9k.post;
 
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
-import java.time.LocalDateTime;
 
 public class UpdatePostRequest {
     @NotBlank
