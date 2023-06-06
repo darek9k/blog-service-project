0af7d0566a12f549ace465878b1203718c84f2fb
darek9k <darecki9k@gmail.com>
2023-06-06 07:12:13 +0200

    Implementation of the specification in the Invoice entity.

--- src/main/java/darek9k/invoice/InvoiceController.java
+++ src/main/java/darek9k/invoice/InvoiceController.java
@@ -2,6 +2,7 @@ package darek9k.invoice;
 
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,10 +46,15 @@ public class InvoiceController {
 
     @GetMapping
     public ResponseEntity<Page<FindInvoiceResponse>> find(@RequestParam(value = "s", defaultValue = "") String seller,
-                                                         @RequestParam(value = "b", defaultValue = "") String buyer,
-                                                         @RequestParam int page,
-                                                         @RequestParam int size) {
+                                                          @RequestParam(value = "b", defaultValue = "") String buyer,
+                                                          @RequestParam int page,
+                                                          @RequestParam int size) {
         return ResponseEntity.ok(invoiceService.find(seller, buyer, page, size));
+    }
+
+    @PostMapping("/find")
+    public ResponseEntity<Page<FindInvoiceResponse>> find(Pageable pageable) {
+        return ResponseEntity.ok(invoiceService.find(pageable));
     }
 
 }
--- src/main/java/darek9k/invoice/InvoiceRepository.java
+++ src/main/java/darek9k/invoice/InvoiceRepository.java
@@ -3,6 +3,7 @@ package darek9k.invoice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
-public interface InvoiceRepository extends CrudRepository<Invoice, Long> {
+public interface InvoiceRepository extends CrudRepository<Invoice, Long>, JpaSpecificationExecutor<Invoice> {
 
     @Query("select i from Invoice i where i.status in :invoiceStatuses and i.seller ilike %:seller% and i.buyer ilike %:buyer%")
     Page<Invoice> findByStatusAndSellerAndBuyer(
