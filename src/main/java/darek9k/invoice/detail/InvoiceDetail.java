48b29be6ed96e71eb79cc849a603e957033c5982
darek9k <darecki9k@gmail.com>
2023-06-09 13:46:19 +0200

    Adding the commentDetail entity

---
+++ src/main/java/darek9k/invoice/detail/InvoiceDetail.java
@@ -0,0 +1,115 @@
+package darek9k.invoice.detail;
+
+import darek9k.invoice.InvoiceStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+public class InvoiceDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Version
+    @NotNull
+    private Integer version;
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @NotNull
+    private LocalDateTime lastModifiedDate;
+    @NotBlank
+    @NotNull
+    @Size(max = 100)
+    private String productName;
+
+    @NotNull
+    private BigDecimal price;
+
+    private Long invoiceId;
+
+    public InvoiceDetail(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Long invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceDetail{" +
+                "id=" + id +
+                ", version=" + version +
+                ", createdDateTime=" + createdDateTime +
+                ", lastModifiedDate=" + lastModifiedDate +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", invoiceId=" + invoiceId +
+                '}';
+    }
+}
