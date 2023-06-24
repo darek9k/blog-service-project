60a87dcabd66b53579095a1b14cce28f563774fd
darek9k <darecki9k@gmail.com>
2023-06-24 06:37:23 +0200

    @Builder(toBuilder = true) Comment

--- src/main/java/darek9k/comment/Comment.java
+++ src/main/java/darek9k/comment/Comment.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -15,6 +16,12 @@ import java.time.LocalDateTime;
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Audited
+@Data
+@NoArgsConstructor
+@ToString(exclude = {"post"})
+@EqualsAndHashCode(exclude = {"post"})
+@Builder(toBuilder = true)
+@AllArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,84 +45,4 @@ public class Comment {
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private Post post;
-
-    public Comment() {
-    }
-
-    public Comment(String text, String author, Post post) {
-        this.text = text;
-        this.author = author;
-        this.post = post;
-    }
-
-    public Comment(Comment comment){
-        this.id = comment.id;
-        this.createdDateTime = comment.createdDateTime;
-        this.lastModifiedDate = comment.lastModifiedDate;
-        this.text = comment.text;
-        this.author = comment.author;
-        this.post = comment.post;
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    public LocalDateTime getCreatedDateTime() {
-        return createdDateTime;
-    }
-
-    public void setCreatedDateTime(LocalDateTime createdDateTime) {
-        this.createdDateTime = createdDateTime;
-    }
-
-    public LocalDateTime getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
-        this.post = post;
-    }
-
-    @Override
-    public String toString() {
-        return "Comment{" +
-                "id=" + id +
-                ", createdDateTime=" + createdDateTime +
-                ", lastModifiedDate=" + lastModifiedDate +
-                ", text='" + text + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-    }
 }
--- src/main/java/darek9k/comment/ReadCommentResponse.java
+++ src/main/java/darek9k/comment/ReadCommentResponse.java
@@ -1,48 +1,23 @@
 package darek9k.comment;
 
 import darek9k.post.ReadPostResponse;
+import lombok.Value;
 
 import java.time.LocalDateTime;
 
+@Value
 public class ReadCommentResponse {
 
-    private final Long id;
+    Long id;
 
-    private final String text;
+    String text;
 
-    private final LocalDateTime createdDateTime;
+    LocalDateTime createdDateTime;
 
-    private final String author;
+    String author;
 
-    private final ReadPostResponse post;
+    ReadPostResponse post;
 
-    public ReadCommentResponse(Long id, String text, LocalDateTime createdDateTime, String author, ReadPostResponse post) {
-        this.id = id;
-        this.text = text;
-        this.createdDateTime = createdDateTime;
-        this.author = author;
-        this.post = post;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public LocalDateTime getCreatedDateTime() {
-        return createdDateTime;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public ReadPostResponse getPost() {
-        return post;
-    }
 
     public static ReadCommentResponse from(Comment comment) {
         return new ReadCommentResponse(
