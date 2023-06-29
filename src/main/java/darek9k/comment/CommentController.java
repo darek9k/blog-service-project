c8c47073b7069b3507df5092b96355b1c57992f5
darek9k <darecki9k@gmail.com>
2023-06-29 07:04:26 +0200

    Building filters using HttpServletRequest and implementing the doFilterInternal method after extending it with OncePerRequestFilter.

--- src/main/java/darek9k/comment/CommentController.java
+++ src/main/java/darek9k/comment/CommentController.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-
 @RestController
 @RequestMapping("/api/comments")
 @RequiredArgsConstructor
--- src/main/java/darek9k/tracking/TrackingFilter.java
+++ src/main/java/darek9k/tracking/TrackingFilter.java
@@ -1,22 +1,43 @@
 package darek9k.tracking;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-@Slf4j
-public class TrackingFilter implements Filter{
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.info("Start request: {}", request);
 
+@Slf4j
+public class TrackingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info(prepareRequestLog(request));
         long start = System.currentTimeMillis();
 
-        chain.doFilter(request,response);
+        filterChain.doFilter(request, response);
 
         long end = System.currentTimeMillis();
-        log.info("Stop request {}, took {} ms", request,(end-start));
+        log.info("Stop request {} {},status {}, took {} ms",
+                request.getMethod(),
+                request.getRequestURI(),
+                response.getStatus(),
+                (end - start));
+    }
+
+    @NotNull
+    private static String prepareRequestLog(HttpServletRequest request) {
+        StringBuilder requestLog = new StringBuilder("Start request: ");
+
+        requestLog.append(request.getMethod())
+                .append(request.getRequestURI());
+
+        if (request.getQueryString() != null) {
+            requestLog.append("?").append(request.getQueryString());
+        }
+
+        return requestLog.toString();
     }
 }
