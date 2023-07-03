0cc5d2191c838aaed8814181e312c672a9c6750e
darek9k <darecki9k@gmail.com>
2023-07-03 05:15:49 +0200

    Introduction of a new filter that is conditionally activated.

---
+++ src/main/java/darek9k/tracking/SeparatorFilter.java
@@ -0,0 +1,24 @@
+package darek9k.tracking;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class SeparatorFilter extends OncePerRequestFilter {
+
+    private static final String SEPARATOR_HEADER_NAME = "separator";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getHeader(SEPARATOR_HEADER_NAME) != null) {
+            log.info("-----------------------------------");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
