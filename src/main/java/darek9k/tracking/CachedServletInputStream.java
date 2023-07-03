40f491b376494ab9d92b9b13455a3ea0f32237a1
darek9k <darecki9k@gmail.com>
2023-07-03 06:29:07 +0200

    Passing parameters from body to login. Using an overloaded implementation of the getInputStream method on request which allows multiple reads - body content. CachedServletInputStream MultiReadHttpServletRequest

---
+++ src/main/java/darek9k/tracking/CachedServletInputStream.java
@@ -0,0 +1,35 @@
+package darek9k.tracking;
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class CachedServletInputStream extends ServletInputStream {
+
+    private ByteArrayInputStream inputStream;
+
+    public CachedServletInputStream(byte[] body) {
+        this.inputStream = new ByteArrayInputStream(body);
+    }
+
+    @Override
+    public int read() throws IOException {
+        return inputStream.read();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return inputStream.available() == 0;
+    }
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void setReadListener(ReadListener readListener) {
+        throw new UnsupportedOperationException();
+    }
+}
